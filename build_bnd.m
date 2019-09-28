cbe4a090d4e9ab7745a7cf5417dd91927f4b6342
RICHARDOIII <32181854+RICHARDOIII@users.noreply.github.com>
2019-09-28 00:06:58 -0500

    edit

--- build_bnd.m
+++ build_bnd.m
@@ -3,5 +3,9 @@ function [B,N,fn] = build_bnd(Param,Grid,I)
 % date: 9/27/2019
 % Description:% This function computes 
 %the operators and r.h.s vectors for both Dirichlet% and Neumann boundary conditions.
+
+
+
+eye(2);
 end
 
